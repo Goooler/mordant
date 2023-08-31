aaaded8692fc1c001833f50a2aed3b2c2b12c303
Goooler <wangzongler@gmail.com>
2023-08-31 20:18:41 +0800

    Move kotlin.multiplatform into catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import java.io.ByteArrayOutputStream
 
 plugins {
-    kotlin("multiplatform").version(libs.versions.kotlin).apply(false)
-    alias(libs.plugins.dokka).apply(false)
-    alias(libs.plugins.publish).apply(false)
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.publish) apply false
 }
 
 val VERSION_NAME: String by project
